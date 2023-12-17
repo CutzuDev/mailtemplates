d48d2c504d466465c55270aea511e9f56c2b95a3
CutzuDev <alexfarkas05@gmail.com>
2023-12-17 18:32:22 +0200

    updt

--- react-email-starter/emails/test.tsx
+++ react-email-starter/emails/test.tsx
@@ -18,16 +18,21 @@ export default function test() {
     <Html>
       <Preview>WIP</Preview>
       <Tailwind>
-      <Head />
-        <Body style={main} className="m-0 h-screen dark:bg-[#F8F8FC] bg-[#F8F8FC]">
-          <Container className="mx-auto p-5 h-full">
+        <Head>
+          <meta name="color-scheme" content="only light"></meta>
+        </Head>
+        <Body
+          style={main}
+          className="m-0 h-screen !bg-[#F8F8FC] dark:!bg-[#F8F8FC]"
+        >
+          <Container className="mx-auto h-full p-5">
             <Img
               src="https://i.imgur.com/1JVNRM1.png"
               className="mx-auto mb-10 w-[300px] object-contain"
             />
             <Container className="overflow-hidden rounded-t-xl">
               <Container className="bg-[url(https://i.imgur.com/1iDjcek.png)] bg-cover">
-                <Text className="my-10 text-center sm:text-5xl text-3xl font-bold text-white">
+                <Text className="my-10 text-center text-3xl font-bold !text-white sm:text-5xl">
                   Hey there!
                 </Text>
               </Container>
@@ -35,7 +40,7 @@ export default function test() {
                 <Heading as="h2" className="m-0">
                   {`Hey {username},`}
                 </Heading>
-                <Text className="text-xl font-light text-slate-700">
+                <Text className="text-xl font-light !text-slate-700">
                   Thanks for signing up!
                   <br />
                   Your application is now under review. You will be notified of
@@ -43,8 +48,8 @@ export default function test() {
                 </Text>
               </Container>
             </Container>
-            <Container className="bg-[#101010] dark:bg-[#101010] overflow-hidden rounded-b-xl p-5 text-white">
-              <Row align="center" className="w-min mt-5">
+            <Container className="overflow-hidden rounded-b-xl !bg-[#101010] p-5 !text-white dark:!bg-[#101010]">
+              <Row align="center" className="mt-5 w-min">
                 <Column align="center" className="w-min px-5">
                   <Img src="https://i.imgur.com/7o6GWyY.png" />
                 </Column>
@@ -55,7 +60,7 @@ export default function test() {
                   <Img src="https://i.imgur.com/5LnoXSV.png" />
                 </Column>
               </Row>
-              <Row className="w-min my-5">
+              <Row className="my-5 w-min">
                 <Column className="w-min px-1">
                   <Link
                     className="text-white underline"
@@ -65,11 +70,11 @@ export default function test() {
                   </Link>
                 </Column>
                 <Column className="w-min px-1">
-                  <Text className="font-semibold my-0">|</Text>
+                  <Text className="my-0 font-semibold">|</Text>
                 </Column>
                 <Column className="w-min px-1">
                   <Link
-                    className="whitespace-nowrap text-white underline"
+                    className="whitespace-nowrap !text-white underline"
                     href=""
                   >
                     Terms & Conditions
@@ -77,11 +82,13 @@ export default function test() {
                 </Column>
               </Row>
               <Container className="px-10">
-                <Container className="h-[1px] bg-neutral-600 dark:bg-neutral-600" />
+                <Container className="h-[1px] !bg-neutral-600 dark:!bg-neutral-600" />
               </Container>
-              <Container align="center" className="my-5 text-neutral-400">
-                <Text className="text-center my-0">All rights reserved 2024 Side Kicks</Text>
-                <Text className="text-center my-0">
+              <Container align="center" className="my-5 !text-neutral-400">
+                <Text className="my-0 text-center">
+                  All rights reserved 2024 Side Kicks
+                </Text>
+                <Text className="my-0 text-center">
                   Sidekicks Store Limited, 99-101 The Horsefair, BS1 3JR | VAT
                   number: N/A
                 </Text>
